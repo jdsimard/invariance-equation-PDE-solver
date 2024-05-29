eb9130f4264b254babb180922712618fcdbee3fa
Joel D. Simard <joel.simard93@gmail.com>
2024-05-29 16:18:35 +0100

    Fixed file initialization issue with writematrix on older versions of MATLAB
    Ran into an issue found on MATLAB R2021a where writematrix of an empty matrix caused an error. The intentional behaviour was that an empty file would be created, so we accomplish this using a file handle now instead.
    
    Fixed spacing in get_multiset_combinations.m.

--- MATLAB/+symbolic_pde_solver/+internal_utils/+file_io/save_solution_files.m
+++ MATLAB/+symbolic_pde_solver/+internal_utils/+file_io/save_solution_files.m
@@ -1,7 +1,6 @@
 function [consistency_state] = save_solution_files(nu,m,n,known_degree,coefficients_matrix,filename_properties,filename_coefficients)
 %SAVE_SOLUTION_FILES Summary of this function goes here
 %   Detailed explanation goes here
-  
   % check consistency of the solution properties and coefficients
   if symbolic_pde_solver.internal_utils.input_verification.consistent_coeff_matrix_and_props(size(coefficients_matrix,1),size(coefficients_matrix,2),nu,n,known_degree) == false
     % the solution properties and coefficients are inconsistent, don't
@@ -10,7 +9,16 @@ function [consistency_state] = save_solution_files(nu,m,n,known_degree,coefficie
   else
     % the properties are consistent, write the files
     writecell(cat(1,{'nu',nu},{'m',m},{'n',n},{'known_degree',known_degree}),filename_properties);
-    writematrix(coefficients_matrix,filename_coefficients);
+    if known_degree == 0
+      %ran into an issue found on MATLAB R2021a where writematrix of an
+      %empty matrix caused an error. The intentional behaviour was that an
+      %empty file would be created, so we accomplish this using a file
+      %handle now instead
+      fid = fopen(filename_coefficients, 'w');
+      fclose(fid);
+    else
+      writematrix(coefficients_matrix,filename_coefficients);
+    end
     consistency_state = true;
   end
 end
--- MATLAB/+symbolic_pde_solver/+internal_utils/+multiset/get_multiset_combinations.m
+++ MATLAB/+symbolic_pde_solver/+internal_utils/+multiset/get_multiset_combinations.m
@@ -5,32 +5,31 @@ function [multiset_combinations_matrix] = get_multiset_combinations(num_vars,tot
   %Use recursion to generate a matrix with rows containing all multisets m 
   % having entries that are non-negative integers such that the entries sum
   % to the total degree
-	%
-	%num_vars: the number of indices for the multiset
-	%total_degree: total degree (sum of powers) in a monomial term
-	
+  %
+  %num_vars: the number of indices for the multiset
+  %total_degree: total degree (sum of powers) in a monomial term
+
   %Note: the number of m's is equivalent to choosing with replacement, so 
   % nchoosek(num_vars + total_degree - 1, total_degree) = (num_vars + total_degree - 1)! / ( total_degree! (num_vars - 1)!)
 
   num_multisets = symbolic_pde_solver.internal_utils.multiset.num_monomials_of_order_d(num_vars,total_degree);
   multiset_combinations_matrix = zeros(num_multisets,num_vars);
-	if num_vars == 1
+  if num_vars == 1
     % if there is only one variable, there is only one multiset combination
     % of the total degree
     multiset_combinations_matrix = total_degree;
   else
     row_count = 0;
-		for i1 = total_degree : -1 : 0
+    for i1 = total_degree : -1 : 0
       % the set of multisets of with num_vars indices and total order d is
       % the union, for all i1 in {1,...,d}, of the set of multisets where
       % the first element is i1 and remaining elements are the sets of
       % multisets of the remaining num_var-1 indices and total order d-i1,
       % hence we can get the desired result recursively
-			tail = symbolic_pde_solver.internal_utils.multiset.get_multiset_combinations(num_vars-1,total_degree-i1);
+      tail = symbolic_pde_solver.internal_utils.multiset.get_multiset_combinations(num_vars-1,total_degree-i1);
       num_rows_to_add = size(tail,1);
       multiset_combinations_matrix(row_count+1:row_count+num_rows_to_add,:) = [i1*ones(size(tail,1),1),tail];
       row_count = row_count + num_rows_to_add;
-		end
-	end
+    end
+  end
 end
-
